46b231f666e5898b710df5ab827bb7a0b62ac888
JOSÉ AZARÍAS PÉREZ TORRES <jose_a_torres@estudante.sesisenai.org.br>
2026-05-12 21:39:45 -0300

    feat: Implementa home no site web

--- src/app/page.js
+++ src/app/page.js
@@ -2,64 +2,59 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.js file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+     <main className="relative min-h-screen bg-zinc-950 text-white overflow-hidden ">
+      <header className="border-b border-zinc-800">
+        <div className="mx-auto flex max-w-6xl items-center justify-between px-6 py-5">
+          <h1 className="text-2xl font-bold text-[#D4AF37]">DriveFast</h1>
+
+          <nav className="flex gap-5 text-sm text-zinc-200">
+            <a href="#" className="rounded-full hover:bg-yellow-500/10 px-4 py-2 transition-colors duration-300 hover:text-[#D4AF37]">Início</a>
+            <a href="#" className="rounded-full hover:bg-yellow-500/10 px-4 py-2 transition-colors duration-300 hover:text-[#D4AF37]">Veículos</a>
+            <a href="#" className="rounded-full hover:bg-yellow-500/10 px-4 py-2 transition-colors duration-300 hover:text-[#D4AF37]">Clientes</a>
+            <a href="#" className="rounded-full hover:bg-yellow-500/10 px-4 py-2 transition-colors duration-300 hover:text-[#D4AF37]">Locações</a>
+          </nav>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
+      </header>
+
+      <section className="grid-cols-1 lg:grid-cols-2 mx-auto flex max-w-6xl flex-col items-start px-6 py-24 ">
+        <span className="mb-4 rounded-full bg-yellow-500/10 px-4 py-2 text-sm text-[#D4AF37]">
+           Aluguel de veículos rápido, simples e seguro
+        </span>
+
+        <h2 className="max-w-3xl text-5xl font-bold leading-tight">
+          Encontre o veículo ideal para sua próxima viagem.
+        </h2>
+
+        <p className="mt-6 max-w-2xl text-lg text-zinc-400">
+          Na DriveFast, você aluga carros com praticidade, agilidade e confiança.
+          Escolha o veículo, consulte a disponibilidade e faça sua locação de forma simples.
+        </p>
+
+        <div className="mt-8 flex gap-4">
           <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            href="#"
+            className="rounded-lg bg-[#D4AF37] px-6 py-3 font-semibold text-black text-bold-700 hover:bg-yellow-600"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
+            Ver veículos
+          </a>
+
+          <a
+            href="#"
+            className="rounded-lg border border-zinc-400 px-6 py-3 font-semibold text-zinc-300 hover:bg-zinc-900"
+          >
+            Nova locação
+          </a>
+        </div>
+          <div className="absolute right-0 top-70 flex justify-center lg:justify-end">
+            <img
+              src="/carro-douradoo.png"
+              alt="Carro esportivo dourado"
+              className=" w-full max-w-[350px] object-contain md:max-w-[500px] lg:max-w-[1250px]"
             />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+          </div>
+          
+      </section>
+      
+    </main>
   );
 }
